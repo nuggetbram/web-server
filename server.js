b09ec578b2e23b999a6f61e48a794ea9996c27c7
Liam Bullard <apps@aetherum.com>
2016-02-11 11:35:29 +1100

    First Commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var port = 3000;
+
+var middleware = {
+    requireAuthentification: function(req, res, next) {
+        next();
+    },
+    logger: function(req, res, next) {
+        var date = new Date().toString();
+        console.log(req.method + '' + req.originalUrl + ' at ' + date);
+        next();
+    }
+};
+
+app.use(middleware.logger);
+
+app.get('/about', middleware.requireAuthentification, function(req, res) {
+    res.send('About Us');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function() {
+    console.log('Server Started on port: ' + port);
+});
